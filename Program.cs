c081872f690fa81d1ffd84dc3fc6ffd0e9fae966
Patryk <Patryk@DESKTOP-I6D93MG>
2025-12-19 22:49:09 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simple_Blackjack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
